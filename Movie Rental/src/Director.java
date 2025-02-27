3ac140b7f53e629f0595fbe6ff9ee98752ad90de
Anggerdhismakusuma <kangger97@gmail.com>
2025-02-27 13:42:44 +0700

    Complete Movie and Director class

--- Movie Rental/src/Director.java
+++ Movie Rental/src/Director.java
@@ -1,3 +1,19 @@
 public class Director {
-    
+
+    private String name;
+    private String biography;
+
+    Director(String name, String biography) {
+        this.name = name;
+        this.biography = biography;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
 }
--- Movie Rental/src/Movie.java
+++ Movie Rental/src/Movie.java
@@ -1,3 +1,25 @@
 public class Movie {
     
+    private String title;
+    private String id;
+    private Director director;
+
+    Movie(String title, String id, Director director) {
+        this.title = title;
+        this.id = id;
+        this.director = director;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
 }
