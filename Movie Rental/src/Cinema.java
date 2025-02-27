fa45c8638a9361cc118458f004dfd595a8377e90
Anggerdhismakusuma <kangger97@gmail.com>
2025-02-27 13:47:47 +0700

    Complete class movie

--- Movie Rental/src/Cinema.java
+++ Movie Rental/src/Cinema.java
@@ -1,3 +1,19 @@
 public class Cinema {
     
+    public void addMovie(Movie movie) {
+        System.out.println("Movie added to cinema: " + movie.getTitle());
+    }
+    
+    public void rentMovie(Viewer viewer, Movie movie) {
+        viewer.rentMovie(movie);
+    }
+    
+    public void returnMovie(Viewer viewer, Movie movie) {
+        viewer.returnMovie(movie);
+    }
+    
+    public void displayAvailableMovies() {
+        System.out.println("Movies in Cinema:");
+    }
+
 }
