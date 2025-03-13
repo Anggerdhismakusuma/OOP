c2c464984683760e162b1e1218aba485b7dad759
Anggerdhismakusuma <kangger97@gmail.com>
2025-03-13 11:12:47 +0700

    Add readme.md file

--- Movie Rental/src/Cinema.java
+++ Movie Rental/src/Cinema.java
@@ -1,19 +1,38 @@
+import java.util.*;
+
 public class Cinema {
     
+    private arrayList<Movie> movies = new arrayList<>();
+    private arrayList<Movie> rentedMovies = new arrayList<>();
+
     public void addMovie(Movie movie) {
+        movies.add(movie);
         System.out.println("Movie added to cinema: " + movie.getTitle());
     }
-    
+
     public void rentMovie(Viewer viewer, Movie movie) {
         viewer.rentMovie(movie);
+        rentedMovies.add(movie);
     }
-    
+
     public void returnMovie(Viewer viewer, Movie movie) {
         viewer.returnMovie(movie);
+        rentedMovies.remove(movie);
     }
-    
+
     public void displayAvailableMovies() {
         System.out.println("Movies in Cinema:");
+        for (Movie movie : movies) {
+            System.out.println("Title: " + movie.getTitle());
+        }
+    }
+
+    public arrayList<Movie> getMovies() {
+        return movies;
+    }
+
+    public arrayList<Movie> getRentedMovies() {
+        return rentedMovies;
     }
 
 }
--- Movie Rental/src/Viewer.java
+++ Movie Rental/src/Viewer.java
@@ -1,3 +1,37 @@
 public class Viewer {
-    
+    private String name;
+    private ArrayList<Movie> rentedMovies = new ArrayList<>();
+
+    public Viewer(String name) {
+        this.name = name;
+    }
+
+    public void rentMovie(Movie movie) {
+        rentedMovies.add(movie);
+        System.out.println(name + " rented: " + movie.getTitle());
+    }
+
+    public void returnMovie(Movie movie) {
+        rentedMovies.remove(movie);
+        System.out.println(name + " returned: " + movie.getTitle());
+    }
+
+    public void displayRentedMovies() {
+        System.out.println(name + "'s Rented Movies:");
+        for (Movie movie : rentedMovies) {
+            System.out.println("Title: " + movie.getTitle());
+            System.out.println("ID: " + movie.getId());
+            System.out.println("Director: " + movie.getDirector().getName());
+            System.out.println("Biography: " + movie.getDirector().getBiography());
+            System.out.println("------------------------");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Movie> getRentedMovies() {
+        return rentedMovies;
+    }
 }
